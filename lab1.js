92dd2fb9d6ac016dfba7119408fe3aa9aef4dfc7
Sandy Chow <sandychow@Sandys-MacBook-Pro.local>
2024-09-16 10:29:54 -0700

    edited

---
+++ lab1.js
@@ -0,0 +1,26 @@
+// step 1: const varible and let varible
+// step2: have to create a console.log 
+// step 3: Pick a topic --> Topic is #2
+// step 4: Const petname = "Kyuubi"; let people = "my sister"
+// 
+const petname = "Kyuubi";
+let entity = "feather toy";
+console.log("My weekly activity during the weekend is to play with my cat," + " " + petname + ".");
+console.log(petname + " loves to play with her favourite" + " " + entity + ".");
+
+entity = "My sister";
+console.log(entity + " usually will spend " + 30 + " minutes to play with " + petname + ", then she will go shopping with me afterwards.");
+
+entity = "my sister";
+console.log(petname + " will then go sleep while "+ entity +" and I go shopping.");
+
+console.log("By the end of the day," + petname + " will welcome " + entity + " and I back home.");
+
+entity = "canned food"
+
+console.log("We will make " + petname + " his favourite " + entity + ".");
+
+console.log(petname+ " will eat very fast and finish the " + entity+ " in "+ 15 + " minutes.");
+
+console.log("This will make my family a happy relax day!")
+
